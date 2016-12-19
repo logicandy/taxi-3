37030e0f4b66eeb6b06ad2d6301b2b6b63c35af9
ivansamoyloff <ivan.samoyloff@gmail.com>
2016-12-19 13:53:58 +0200

    fixed for pull request v1.1

--- src/config/RouterConfig.js
+++ src/config/RouterConfig.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import OrderPage from '../components/OrderPage/OrderPage';
-import SignUpForm from '../components/SignUpForm/SignUpForm';
+import SignUp from '../components/SignUpPage/SignUpPage';
 import Admin from '../components/Admin/Admin';
 import DriversOrder from '../components/Driver/DriversOrder/DriversOrder';
 import Dispatcher from '../components/Dispatcher/Dispatcher';
@@ -10,7 +10,7 @@ import Orders from '../components/Orders/Orders';
 const RouterConfig = (
   <Router history={browserHistory}>
     <Route path='/' component={OrderPage}/>
-    <Route path='/signupform' component={SignUpForm}/>
+    <Route path='/signup' component={SignUp}/>
     <Route path='/admin' component={Admin}/>
     <Route path='/drivers/order' component={DriversOrder}/>
     <Route path='/dispatcher' component={Dispatcher}/>
