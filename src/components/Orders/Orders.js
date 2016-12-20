acb2f8591a18035b1480197efa3f2888948c7eb7
ivansamoyloff <ivan.samoyloff@gmail.com>
2016-12-20 17:45:08 +0200

    fixed for pull request v1.2

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -6,13 +6,13 @@ import Orderslist from './Orderslist.js';
 export default class Orders extends React.Component {
   render() {
     return (
-            <div className="Orders">
-            <div className="Orders-header">
-              <img src={logo} className="Orders" alt="logo"/>
-              <h2>Welcome to Sloboda taxi</h2>
-            </div>
-            <Orderslist />
-            </div>
+      <div className="Orders">
+      <div className="Orders-header">
+        <img src={logo} className="Orders" alt="logo"/>
+        <h2>Welcome to Sloboda taxi</h2>
+      </div>
+      <Orderslist />
+      </div>
     );
   }
 }
