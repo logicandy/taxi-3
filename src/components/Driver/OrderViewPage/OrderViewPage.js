3267ca35d6c792c89d79b6ec721e7fe9824f42c7
Мира <myroslava.yaremenko@nure.ua>
2016-12-21 12:12:35 +0200

    Created component for order details, fixed problem with hint message

---
+++ src/components/Driver/OrderViewPage/OrderViewPage.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './OrderViewPage.css';
+import OrderDetails from '../OrderDetails/OrderDetails';
+import {browserHistory} from 'react-router';
+import  {blank, existing, existingCompleted} from '../../../fixtures/order';
+import api from '../../../modules/api';
+
+export default class OrderViewPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      order: null
+    };
+    this.backToOrders = this.backToOrders.bind(this);
+  }
+
+  componentWillMount() {
+ /*   api.getOrder(this.props.params.id).then((order)=> {
+      this.setState({
+        order: order
+      });
+    });
+*/
+    this.setState({
+      order: {
+        order_id: this.props.params.id,
+        ...existing
+      }
+    });
+
+  }
+
+  backToOrders() {
+    browserHistory.push('/drivers/order');
+  }
+
+  render() {
+    return (
+      <div>
+        <OrderDetails
+          order={this.state.order}
+        />
+        <div className="order-view--footer">
+          <button
+            onClick={this.backToOrders}
+            className="btn order-view--back-button">
+            Back to orders
+          </button>
+          <button
+            className="btn btn-primary order-view--accept-button">
+            Accept order
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
--- src/config/RouterConfig.js
+++ src/config/RouterConfig.js
@@ -5,7 +5,7 @@ import SignUp from '../components/SignUpPage/SignUpPage';
 import Admin from '../components/Admin/Admin';
 import DriversOrder from '../components/Driver/DriversOrder/DriversOrder';
 import Dispatcher from '../components/Dispatcher/Dispatcher';
-import OrderView from '../components/Driver/OrderView/OrderView';
+import OrderViewPage from '../components/Driver/OrderViewPage/OrderViewPage';
 
 const RouterConfig = (
   <Router history={browserHistory}>
@@ -14,7 +14,7 @@ const RouterConfig = (
     <Route path='/admin' component={Admin}/>
     <Route path='/drivers'>
       <Route path='order' component={DriversOrder}/>
-      <Route path="order/:id" component={OrderView}/>
+      <Route path="order/:id" component={OrderViewPage}/>
     </Route>
     <Route path='/dispatcher' component={Dispatcher}/>
   </Router>
