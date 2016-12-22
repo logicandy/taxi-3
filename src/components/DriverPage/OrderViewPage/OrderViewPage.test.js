4971be6be1c2d47498e9f498be640f4b67027be9
Мира <myroslava.yaremenko@nure.ua>
2016-12-22 19:16:41 +0200

    Fixed test for OrderViewPage

--- src/components/DriverPage/OrderViewPage/OrderViewPage.test.js
+++ src/components/DriverPage/OrderViewPage/OrderViewPage.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import OrderViewPage from './OrderViewPage';
+import api from '../../../modules/api';
+import  {order} from '../../../fixtures/orders';
 
 
 const paramsId = {id: 12};
 
 it('renders without crashing', () => {
+  api.getOrder = () =>  Promise.resolve(order);
   const div = document.createElement('div');
   ReactDOM.render(
     <OrderViewPage
